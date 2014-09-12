edd5b49b5278201674a95137695c30d70aeb987d
Rahul <rahulroot@gmail.com>
2014-09-12 10:10:47 -0700

    added code for Uva:10220

---
+++ 10220/program.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+int fact[1001][3000];
+void print(int n)
+{
+	int len = fact[n][0];
+	int j=0, sum=0;
+	for(j=len; j > 0; --j)
+	{
+		sum += fact[n][j];
+	}
+	printf("%d\n", sum );
+}
+void calc_fact(void)
+{
+	/* fact(0) = 1 */
+	fact[0][0] = 1; /* length of array fact[1] */
+	fact[0][1] = 1; /* entry of array fact[1] */
+	int i;
+	for(i=1; i <= 1000; ++i)
+	{
+		int carry = 0;
+		int j=1, tmp=0;
+		for(j; (j <= fact[i-1][0]) || (carry) ; ++j)
+		{
+			int digit = (fact[i-1][j] * i ) + carry;
+			fact[i][j] = digit % 10;
+			carry = digit / 10;
+			tmp++;
+			fact[i][0] = tmp;
+		}
+	}
+}
+int main()
+{
+	calc_fact();
+  freopen("./sample.txt", "r", stdin);
+	int n;
+	while( scanf("%d", &n) == 1) 
+	{
+		if( n > 1000)
+			continue;
+		else
+			print(n);	
+	}
+	return 0;
+}
+
+
